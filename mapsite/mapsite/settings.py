d060f3ce8ed65280622f4f5187377c3cad829591
salte2ba <salte2ba@cmich.edu>
2014-11-17 09:44:17 -0500

    changed file path

--- mapsite/mapsite/settings.py
+++ mapsite/mapsite/settings.py
@@ -84,3 +84,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIRS= ("C:/Users/salte2ba/projects/independent_project/independent_project/mapsite/templates/staticsite")
